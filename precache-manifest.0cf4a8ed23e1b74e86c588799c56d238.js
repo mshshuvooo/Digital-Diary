6979bb5deebbd34f097166040a554b4c48a016bd
Shahadat Shuvo <shuvo@Mohammads-MacBook-Pro.local>
2019-10-28 23:44:27 +0600

    Updates

---
+++ precache-manifest.0cf4a8ed23e1b74e86c588799c56d238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080dcc11d65708ba098f5c81434cde94",
+    "url": "/digital-diary/index.html"
+  },
+  {
+    "revision": "d80f9e888c4ae1f7b348",
+    "url": "/digital-diary/static/css/main.bb6c0926.chunk.css"
+  },
+  {
+    "revision": "629cdcc01d9369243f0f",
+    "url": "/digital-diary/static/js/2.7c6d1866.chunk.js"
+  },
+  {
+    "revision": "d80f9e888c4ae1f7b348",
+    "url": "/digital-diary/static/js/main.466c82cc.chunk.js"
+  },
+  {
+    "revision": "882210de49e0ac3124e6",
+    "url": "/digital-diary/static/js/runtime-main.8826a641.js"
+  },
+  {
+    "revision": "74e8fcb3780a4e4c8634d484d472bde2",
+    "url": "/digital-diary/static/media/loading.74e8fcb3.gif"
+  }
+]);
